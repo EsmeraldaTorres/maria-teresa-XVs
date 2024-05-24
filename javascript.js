2c37cee3730a15020f61df585087360207b6306f
Esmeralda Torres Aguirre <esmeraldatorresaguirre@MacBook-Air-de-Esmeralda.local>
2024-05-23 23:26:18 -0600

    modify date

--- javascript.js
+++ javascript.js
@@ -36,7 +36,7 @@ playPause.addEventListener("click", () => {
 
 // a regresiva -------------------------------------------------------------------
 // Set the date we're counting down to
-var countDownDate = new Date("Apr 27, 2024 18:00").getTime();
+var countDownDate = new Date("Jul 27, 2024 18:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function () {
